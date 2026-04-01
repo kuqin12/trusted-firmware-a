78622085682b99e400898143162ef4fa3a6b139d
Kun Qin <kuqin@microsoft.com>
2026-04-01 16:56:10 -0700

    support suspend for qemu

--- plat/qemu/common/qemu_pm.c
+++ plat/qemu/common/qemu_pm.c
@@ -157,7 +157,17 @@ qemu_pwr_domain_pwr_down_wfi(const psci_power_state_t *target_state)
  ******************************************************************************/
 void qemu_pwr_domain_suspend(const psci_power_state_t *target_state)
 {
-	assert(0);
+	// Set up the mailbox to prepare for the interrupt coming back up
+	unsigned pos = plat_my_core_pos();
+	struct hold_slot *hold_base = (struct hold_slot *)PLAT_QEMU_HOLD_BASE;
+
+	// As we enter the suspend state, set the entry field to HOLD_STATE_SUSPEND
+	// to indicate the core is suspended.
+	hold_base[pos].magic1 = PLAT_QEMU_HOLD_SLOT_MAGIC1;
+	hold_base[pos].magic2 = PLAT_QEMU_HOLD_SLOT_MAGIC2;
+	hold_base[pos].entry = secure_entrypoint;
+
+	flush_dcache_range((uintptr_t)&hold_base[pos], sizeof(struct hold_slot));
 }
 
 /*******************************************************************************
@@ -180,7 +190,7 @@ void qemu_pwr_domain_on_finish(const psci_power_state_t *target_state)
  ******************************************************************************/
 void qemu_pwr_domain_suspend_finish(const psci_power_state_t *target_state)
 {
-	assert(0);
+	// Do nothing
 }
 
 /*******************************************************************************
